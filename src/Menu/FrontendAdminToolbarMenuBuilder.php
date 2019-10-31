61db99a4aaa53d6854d50b9d03fbc83c20badcae
Néstor de Dios Fernández <nestordedios@outlook.es>
2019-10-31 12:11:01 +0100

    Add constructor for FrontendAdminToolbarMenuBuilder

--- src/Menu/FrontendAdminToolbarMenuBuilder.php
+++ src/Menu/FrontendAdminToolbarMenuBuilder.php
@@ -2,10 +2,26 @@
 
 namespace NestordeDios\Menu;
 
+use Knp\Menu\FactoryInterface;
+
 final class FrontendAdminToolbarMenuBuilder implements FrontendAdminToolbarMenuBuilderInterface
 {
-    public function _construct()
-    {
+    /** @var FactoryInterface */
+    private $menuFactory;
 
+    /** @var UrlGeneratorInterface */
+    private $urlGenerator;
+
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(
+        FactoryInterface $menuFactory,
+        UrlGeneratorInterface $urlGenerator,
+        TranslatorInterface $translator
+    ) {
+        $this->menuFactory = $menuFactory;
+        $this->urlGenerator = $urlGenerator;
+        $this->translator = $translator;
     }
 }
